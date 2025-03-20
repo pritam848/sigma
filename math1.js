8e35e0fd22f1080e1b306a6e6fb6755fb3b8153b
pritam848 <pritampathade1@gmail.com>
2025-03-20 16:09:50 +0530

    added random method

---
+++ math1.js
@@ -0,0 +1,18 @@
+// math object constants
+console.log("the value of pi is "+Math.PI);
+console.log("the square root of 2 is "+ Math.SQRT2);
+console.log("the sqrt of 1/2 is "+Math.SQRT1_2);
+//math object methods
+console.log("the round value of 25.66 is"+Math.round(25.66));
+console.log("the round value of 25.4 is"+Math.round(25.4));
+console.log("the ceil value of 44.3 is"+Math.ceil(44.3));
+console.log("the floor value of 52.7 is"+Math.floor(52.7));
+//power and root method
+console.log("the square root of 4 is "+Math.sqrt(4));
+console.log("the value of 4 raise to 2 is"+Math.pow(4,2));
+console.log("the cube root of 9 is "+Math.cbrt(9));
+
+// generating random values
+console.log("random value between 0 and 1 is "+Math.random());
+console.log("the random values between 0 to 100 is "+Math.floor(Math.random()*100));
+console.log("the random values between 100 and 1000 "+Math.floor(Math.random()*(1000-100+1)+100));
